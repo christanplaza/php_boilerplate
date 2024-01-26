412938507464e5f39157f0a962dade2d1f295a76
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2024-01-26 12:20:21 +0800

    Initial Template

---
+++ sample_user_db.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'user') DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
